ba4ed653e2208850dee66d4f875af1c511ddb198
Andrey Azov <andrey.azov@gmail.com>
2017-12-12 01:46:57 +0300

    Day 10, part 1

--- src/day10.js
+++ src/day10.js
@@ -2,12 +2,51 @@
 import puzzleInput from '../data/day10';
 
 import range from 'ramda/src/range';
+import zip from 'ramda/src/zip';
+import reverse from 'ramda/src/reverse';
 
 /* PART 1 */
 function generateList() {
-  return range(0, 255);
+  return range(0, 256);
 }
 
 export function reorderList(list, reorderLengths) {
+  let cursorIndex = 0;
 
+  for (let i = 0; i < reorderLengths.length; i++) {
+    list = reorderSection(list, cursorIndex, reorderLengths[i]);
+    cursorIndex = (cursorIndex + reorderLengths[i] + i) % list.length;
+  }
+
+  return list;
 }
+
+function reorderSection(list, currentIndex, number) {
+  let indices = [];
+  let values = [];
+
+  for (let i = 0; i < number; i++) {
+    let index = (currentIndex + i) % list.length;
+    indices.push(index);
+    values.push(list[index])
+  }
+
+  values = reverse(values);
+  indices.forEach((index, position) => {
+    list[index] = values[position];
+  });
+  return list;
+}
+
+export function validateReorderedList(list) {
+  return list[0] * list[1];
+}
+
+function solvePart1() {
+  let list = generateList();
+  let reorderLengths = puzzleInput.split(',').map(Number);
+  let reorderedList = reorderList(list, reorderLengths);
+  return validateReorderedList(reorderedList);
+}
+
+console.log('solution of the first part of the puzzle is', solvePart1());
--- tests/day10.spec.js
+++ tests/day10.spec.js
@@ -1,4 +1,7 @@
-import { reorderList } from '../src/day10';
+import {
+  reorderList,
+  validateReorderedList
+} from '../src/day10';
 
 describe('reorderList', () => {
 
@@ -8,9 +11,20 @@ describe('reorderList', () => {
 
     let expectedList = [3, 4, 2, 1, 0];
 
-    console.log(reorderList(sampleList, reorderLengths));
-
-    expect(true).toBe(true);
+    expect(reorderList(sampleList, reorderLengths)).toEqual(expectedList);
   });
 
 });
+
+describe('validate reordered list', () => {
+
+  test('checks checksum of sample reordered list', () => {
+    let sampleList = [0, 1, 2, 3, 4];
+    let reorderLengths = [3, 4, 1, 5];
+
+    let reorderedList = reorderList(sampleList, reorderLengths);
+
+    expect(validateReorderedList(reorderedList)).toEqual(12);    
+  })
+
+})
